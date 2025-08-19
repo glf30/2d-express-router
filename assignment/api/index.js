6df37b335138c691a9e43354adf725f718321949
Greyson <greyson.frazier@gmail.com>
2025-08-19 19:55:45 -0400

    Delete assignment/api directory

--- assignment/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.use("/actors", require("./router/actors-Router"));
-
-router.use("/musicians", require("./router/musicians-Router"));
-
-module.exports = router;
