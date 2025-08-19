37799b8142c7f017d1a9134fbe9d9c4d48f7d27a
Greyson <greyson.frazier@gmail.com>
2025-08-19 19:57:59 -0400

    Update actor-data.js

--- assignment/data/actor-data.js
+++ assignment/data/actor-data.js
@@ -1,25 +1,25 @@
 const { v4: uuid } = require("uuid");
 
-let musicians = [
+let actors = [
   {
     id: uuid(),
-    name: "Carson Pace",
-    age: 29,
-  },
-  {
-    id: uuid(),
-    name: "Anthony Green",
+    name: "Nathan Fielder",
     age: 42,
   },
   {
     id: uuid(),
-    name: "Lizzy McAlpine",
-    age: 25,
+    name: "Emma Stone",
+    age: 36,
   },
   {
     id: uuid(),
-    name: "Yvette Young",
-    age: 33,
+    name: "Aaron Paul",
+    age: 45,
+  },
+  {
+    id: uuid(),
+    name: "Samuel L Jackson",
+    age: 73,
   },
 ];
 // console.log(actors);
