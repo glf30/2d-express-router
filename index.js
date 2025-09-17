a30c7e0bd0ffb6974bb0ed13684f7a7dbc9a8269
Greyson <greyson.frazier@gmail.com>
2025-09-17 16:10:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,11 @@
 /*
     3. Import Router files
 */
-const emojisRouter = require('./routes/emojisRouter');
 
 /*
     4. Set up the URL routes to connect to each router
 */
-// localhost:3000/api/v1/emojis
-app.use("/api/v1/emojis", emojisRouter);
+
 /*
     5. Set up the port and begin listening
 */
